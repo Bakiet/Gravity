c5e0f3fcaa42c081dd083e41562c72b1cab7b7d9
jafasaj <danny.ruta949@gmail.com>
2016-05-12 18:35:58 -0430

    varios
    ...

---
+++ Assets/First Ground/Readme.txt
@@ -0,0 +1,79 @@
+------------------------------------------------------------------
+First Ground 1.3.5
+------------------------------------------------------------------
+	
+	First Ground contains handpainted textures, materials, particles, demo scenes and sample scripts which helps you quickly create amazing Arctic Winter, Grassland, Volcano and Wasteland scenes.
+
+	Features: 
+
+		• 37 Colorful handpainted textures .
+		• 12 Arctic textures.
+		• 9 Grassland textures. 
+		• 9 Volcano textures. 
+		• 7 Wasteland textures. 
+		• 4 Demo scenes.
+		• Fog, snow, smoke, sand particles.
+		• Works with Unity's Terrain painting tools.
+
+		• Support all build player platforms.
+		
+	Compatible:
+
+		• Unity 4.6.9 or higher.
+		• Unity 5.3.2 or higher.
+
+	Product page:
+	
+		https://www.assetstore.unity3d.com/en/#!/content/18442
+
+	Please direct any bugs/comments/suggestions to support e-mail (geteamdev@gmail.com).
+		
+	Thank you for your support,
+
+	Gold Experience Team
+	E-mail: geteamdev@gmail.com
+	Website: http://www.ge-team.com
+
+------------------------------------------------------------------
+Release notes
+------------------------------------------------------------------
+
+	Version 1.3.5
+	
+		• Add Full Screen toggle button.
+		• Add Settings button and details panels.
+		• Fixed GUID conflict with other packages.
+		• Improve Orbit camera controller.
+		• Update sample scripts.
+		• Update Demo scenes.
+		• Unity 4.6.9 and higher compatible.
+		• Unity 5.3.2 and higher compatible.
+
+	Version 1.3.1 (Released on Jul 10, 2015)
+
+		• Add UI to demo scenes.
+		• Add GUI Animator FREE.
+		• Update demo scenes.
+		• Update sample scripts.
+		• Update bake light map.
+		• Unity 5.0.1 and higher compatible.
+
+	Version 1.3.0 (Released on Oct 20, 2014)
+
+		• Add 9 Glassland textures.
+		• Add 9 Volcano textures.
+		• Add 7 Wasteland textures.
+		• Add 3 demo scenes.
+		• Update demo scripts.
+		• Unity 4.5.0 and higher compatible.
+
+	Version 1.0 (Initial version, released on Jun 6, 2014)
+
+		• Colorful handpainted textures .
+		• 5 ice textures.
+		• 3 snow textures.
+		• 2 soil textures.
+		• 2 rock textures.
+		• Fog and snow particles.
+		• Mobile friendly.
+		• Unity 4.2.2 and higher compatible.
---
+++ Assets/First Ground/Scripts/FirstGround_UIController.cs
@@ -0,0 +1,164 @@
+﻿// First Ground 1.3.5
+//
+// Author:	Gold Experience Team (http://www.ge-team.com)
+// Details:	https://www.assetstore.unity3d.com/en/#!/content/18442
+// Support:	geteamdev@gmail.com
+//
+// Please direct any bugs/comments/suggestions to support e-mail.
+
+#region Namespaces
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+#endregion // Namespaces
+
+// ######################################################################
+// FirstGround_UIController class
+// Handles user inputs.
+//
+// Note this class is attached with "Canvas  (Scenes)" object in all demo scenes.
+// ######################################################################
+
+public class FirstGround_UIController : MonoBehaviour
+{
+	// ########################################
+	// Variables
+	// ########################################
+
+	#region Variables
+
+	// Canvas
+	public Canvas m_Canvas = null;
+
+	// SelectDemo
+	public Button m_SelectDemo_Button = null;
+	public GameObject m_SelectDemo_Window = null;
+
+	#endregion //Variables
+
+	// ########################################
+	// MonoBehaviour Functions
+	// http://docs.unity3d.com/ScriptReference/MonoBehaviour.html
+	// ########################################
+
+	#region MonoBehaviour
+
+	// Awake is called when the script instance is being loaded.
+	// http://docs.unity3d.com/ScriptReference/MonoBehaviour.Awake.html
+	void Awake()
+	{
+		// Set GUIAnimSystemFREE.Instance.m_AutoAnimation to false, 
+		// this will let you control all GUI Animator elements in the scene via scripts.
+		if (enabled)
+		{
+			//GUIAnimSystemFREE.Instance.m_GUISpeed = 1.0f;
+			GUIAnimSystemFREE.Instance.m_AutoAnimation = false;
+		}
+	}
+
+	// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
+	// http://docs.unity3d.com/ScriptReference/MonoBehaviour.Start.html
+	void Start()
+	{
+		// Play UI move-in animations
+		StartCoroutine(ShowUIs());
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+	#endregion // MonoBehaviour
+
+	// ########################################
+	// Show UI functions
+	// ########################################
+
+	#region Show UI functions
+
+	// Play UI move-in animations
+	IEnumerator ShowUIs()
+	{
+		// Disable all buttons of m_Canvas
+		// http://docs.unity3d.com/Manual/script-GraphicRaycaster.html
+		GUIAnimSystemFREE.Instance.SetGraphicRaycasterEnable(m_Canvas, false);
+
+		yield return new WaitForSeconds(0.5f);
+
+		GUIAnimSystemFREE.Instance.MoveIn(m_SelectDemo_Button.transform, true);
+
+		yield return new WaitForSeconds(0.25f);
+
+		// Enable all buttons of m_Canvas
+		// http://docs.unity3d.com/Manual/script-GraphicRaycaster.html
+		GUIAnimSystemFREE.Instance.SetGraphicRaycasterEnable(m_Canvas, true);
+	}
+
+	#endregion // Show UI functions
+
+	// ########################################
+	// UI Responders
+	// ########################################
+
+	#region UI Responders
+
+	// Show demo window
+	public void Button_SelectDemo()
+	{
+		GUIAnimSystemFREE.Instance.MoveOut(m_SelectDemo_Button.transform, true);
+		GUIAnimSystemFREE.Instance.MoveIn(m_SelectDemo_Window.transform, true);
+	}
+
+	// Show demo button
+	public void Button_SelectDemo_Minimize()
+	{
+		GUIAnimSystemFREE.Instance.MoveIn(m_SelectDemo_Button.transform, true);
+		GUIAnimSystemFREE.Instance.MoveOut(m_SelectDemo_Window.transform, true);
+	}
+
+	// Open Arctic demo scene
+	public void Button_SelectDemo_Arctic()
+	{
+		GUIAnimSystemFREE.Instance.LoadLevel("First Ground - Arctic (960x600px)", 0.1f);
+	}
+
+	// Open Grassland demo scene
+	public void Button_SelectDemo_Grassland()
+	{
+		GUIAnimSystemFREE.Instance.LoadLevel("First Ground - Grassland (960x600px)", 0.1f);
+	}
+
+	// Open Volcano demo scene
+	public void Button_SelectDemo_Volcano()
+	{
+		GUIAnimSystemFREE.Instance.LoadLevel("First Ground - Volcano (960x600px)", 0.1f);
+	}
+
+	// Open Wasteland demo scene
+	public void Button_SelectDemo_Wasteland()
+	{
+		GUIAnimSystemFREE.Instance.LoadLevel("First Ground - Wasteland (960x600px)", 0.1f);
+	}
+
+	// Support button
+	public void Button_Help_Support()
+	{
+		// http://docs.unity3d.com/ScriptReference/Application.OpenURL.html
+		Application.OpenURL("mailto:geteamdev@gmail.com");
+	}
+
+	// Products button
+	public void Button_Help_Products()
+	{
+		// http://docs.unity3d.com/ScriptReference/Application.ExternalEval.html
+		//Application.ExternalEval("window.open('http://ge-team.com/pages/unity-3d/','GOLD EXPERIENCE TEAM')");
+
+        // http://docs.unity3d.com/ScriptReference/Application.OpenURL.html
+        Application.OpenURL("http://ge-team.com/pages/unity-3d/");
+	}
+
+	#endregion // UI Responders
+}
